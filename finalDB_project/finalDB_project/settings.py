d5243245a9d9806f92e900b817317e8bb0ec68be
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2023-03-08 18:27:29 -0500

    created users app and template

--- finalDB_project/finalDB_project/settings.py
+++ finalDB_project/finalDB_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'animemaster.apps.animemasterConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
