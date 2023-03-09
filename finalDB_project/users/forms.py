4cdbeb90acd8cc127eea1d5b44802530939b2886
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2023-03-08 20:55:01 -0500

    Updated registration form, crispy dont work

--- finalDB_project/users/forms.py
+++ finalDB_project/users/forms.py
@@ -1,6 +1,6 @@
-form django import forms
-form django.contrib.auth.models import User
-form django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
