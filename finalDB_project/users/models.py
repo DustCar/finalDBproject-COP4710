0acbd063ad8cd139884fa289a4d914c96a25be66
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2023-03-08 22:59:40 -0500

    Connected registration to database and started on login

--- finalDB_project/users/models.py
+++ finalDB_project/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=40)
 # Create your models here.
--- finalDB_project/users/views.py
+++ finalDB_project/users/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .models import User
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            new_user = User()
+            new_user.username = form.cleaned_data.get('username')
+            new_user.email = request.POST.get('email') 
+            new_user.save()
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            #username = form.cleaned_data.get('username')
+            #messages.success(request, f'Account created for {username}!')
             return redirect('animemaster-home')
     else:
         form = UserRegisterForm()
