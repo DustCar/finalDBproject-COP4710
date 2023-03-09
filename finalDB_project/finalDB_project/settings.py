8a16752d3a1a09b664bc1e2ae4b13ac435fe0a82
DustCar <dustcarmelo@yahoo.com>
2023-03-09 17:49:16 -0500

    set database to my credentials

--- finalDB_project/finalDB_project/settings.py
+++ finalDB_project/finalDB_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'finalDB_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'animemaster2',
+        'USER': 'dustc',
+        'PASSWORD': 'August__2902',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
