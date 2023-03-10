218530d8577341b948e80d0fba6ef826d8c2338a
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2023-03-09 19:19:16 -0500

    Nothing new

--- finalDB_project/users/models.py
+++ finalDB_project/users/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class User(models.Model):
     username = models.CharField(max_length=30)
     email = models.EmailField()
     password = models.CharField(max_length=40)
-# Create your models here.
+    
+#class Profile(models.Model):
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
